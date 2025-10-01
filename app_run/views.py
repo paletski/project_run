deb43af8b405064575e44ee13170b5de7d8c7bac
paletski <paletski@yandex.ru>
2025-10-02 00:52:29 +0700

    Задача 9. странные проверки - 4 правим диапазон веса

--- app_run/views.py
+++ app_run/views.py
@@ -119,8 +119,8 @@ class AthleteInfoViewSet(APIView):
         if 'weight' in data:
             try:
                 weight = int(data['weight'])  # пока без try
-                if weight < 0 or weight > 899:
-                    message = {'msg': 'вес не в диапазоне 1-899'}
+                if weight <= 0 or weight > 900:
+                    message = {'msg': 'вес не в диапазоне 1-900'}
                     return Response(message, status=status.HTTP_400_BAD_REQUEST)
             # проскочили проверку, в диапазоне и число (помним про try)
             except ValueError:
