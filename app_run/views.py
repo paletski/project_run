c0bc3c76454a154b1747f0669971b3e4580e23c3
paletski <paletski@yandex.ru>
2025-11-03 00:28:34 +0700

    Задача 21.0. Скорость - проверка GPT-codex, считаем через сумма скоростей/n

--- app_run/views.py
+++ app_run/views.py
@@ -150,8 +150,8 @@ class RunStopViewSet(APIView):
                         coll_item.collitems.add(user_id)
 
             # среднее арифм. скорость - проверка gpt-codex
-            #speed_avg = round(speed_summ / (pos_cnt - 1), 2)
-            speed_avg = round(((probeg * 1000) / run_time), 2)
+            speed_avg = round(speed_summ / (pos_cnt - 1), 2)
+            #speed_avg = round(((probeg * 1000) / run_time), 2)
             #speed_avg = round(speed_summ / pos_cnt, 2)
             run.speed =  speed_avg  #f'{speed_avg:.2f}'
             run.distance = probeg
